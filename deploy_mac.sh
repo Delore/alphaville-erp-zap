0586e8c9649eae532649b0d504d2390cf8b22130
Delore <delore23@gmail.com>
2023-04-14 00:05:16 -0300

    Build and Deploy for Mac

--- deploy_mac.sh
+++ deploy_mac.sh
@@ -2,13 +2,13 @@
 echo "Build and Deploy for Mac"
 git add . 
 git commit -m "Build and Deploy for Mac"
-git push origin master
+git push origin main
 
 npm version patch
 
 git add . 
 git commit -m "Bumb version"
-git push origin master
+git push origin main
 
 rm -R ./build
 npm run dist
