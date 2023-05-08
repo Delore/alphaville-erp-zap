b53accfa8bfe6b28c596f2149006498abd9855f0
Rafael G Coelho <delore23@gmail.com>
2023-05-08 19:17:39 -0300

    Build and Deploy for Win

--- deploy_win.sh
+++ deploy_win.sh
@@ -3,18 +3,10 @@ echo "Build and Deploy for Win"
 git add . 
 git commit -m "Build and Deploy for Win"
 git push origin master
-
 npm version patch
-
 git add . 
 git commit -m "Bumb version"
 git push origin master
-
 rm -R ./build
-npm run dist
-
-PACKAGE_VERSION=$(grep '"version":' package.json | cut -d\" -f4)
-
-scp './build/alphaville-erp-zap_'$PACKAGE_VERSION'.exe' root@191.252.219.73:/root/delore-erp-srv/www/site/downloads
-
+npm run deploy
 echo "Finish"
